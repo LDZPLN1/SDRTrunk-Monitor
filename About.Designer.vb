db58950e7173014ecf57876aa03125219e9e2da9
Douglas Graham <ldzpln1@aol.com>
2023-06-15 05:50:50 -0400

    About Fix

--- About.Designer.vb
+++ About.Designer.vb
@@ -14,15 +14,6 @@ Partial Class About
         End Try
     End Sub
 
-    Friend WithEvents TableLayoutPanel As System.Windows.Forms.TableLayoutPanel
-    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
-    Friend WithEvents LabelProductName As System.Windows.Forms.Label
-    Friend WithEvents LabelVersion As System.Windows.Forms.Label
-    Friend WithEvents LabelSource As System.Windows.Forms.Label
-    Friend WithEvents TextBoxLicense As System.Windows.Forms.TextBox
-    Friend WithEvents OKButton As System.Windows.Forms.Button
-    Friend WithEvents LabelCopyright As System.Windows.Forms.Label
-
     'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
@@ -32,15 +23,88 @@ Partial Class About
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(About))
-        TableLayoutPanel = New TableLayoutPanel()
-        LabelProductName = New Label()
-        LabelVersion = New Label()
-        LabelCopyright = New Label()
-        LabelSource = New Label()
-        TextBoxLicense = New TextBox()
         OKButton = New Button()
+        TextBoxDescription = New TextBox()
+        LabelCompanyName = New Label()
+        LabelCopyright = New Label()
+        LabelVersion = New Label()
+        LabelProductName = New Label()
+        TableLayoutPanel = New TableLayoutPanel()
         TableLayoutPanel.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' OKButton
+        ' 
+        OKButton.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        OKButton.DialogResult = DialogResult.Cancel
+        OKButton.Location = New Point(371, 268)
+        OKButton.Margin = New Padding(4, 3, 4, 3)
+        OKButton.Name = "OKButton"
+        OKButton.Size = New Size(88, 27)
+        OKButton.TabIndex = 0
+        OKButton.Text = "&OK"
+        ' 
+        ' TextBoxDescription
+        ' 
+        TextBoxDescription.Dock = DockStyle.Fill
+        TextBoxDescription.Location = New Point(7, 119)
+        TextBoxDescription.Margin = New Padding(7, 3, 4, 3)
+        TextBoxDescription.Multiline = True
+        TextBoxDescription.Name = "TextBoxDescription"
+        TextBoxDescription.ReadOnly = True
+        TextBoxDescription.ScrollBars = ScrollBars.Both
+        TextBoxDescription.Size = New Size(452, 143)
+        TextBoxDescription.TabIndex = 0
+        TextBoxDescription.TabStop = False
+        TextBoxDescription.Text = resources.GetString("TextBoxDescription.Text")
+        ' 
+        ' LabelCompanyName
+        ' 
+        LabelCompanyName.Dock = DockStyle.Fill
+        LabelCompanyName.Location = New Point(7, 87)
+        LabelCompanyName.Margin = New Padding(7, 0, 4, 0)
+        LabelCompanyName.MaximumSize = New Size(0, 20)
+        LabelCompanyName.Name = "LabelCompanyName"
+        LabelCompanyName.Size = New Size(452, 20)
+        LabelCompanyName.TabIndex = 0
+        LabelCompanyName.Text = "Source: https://github.com/LDZPLN1/SDRTrunk-Monitor"
+        LabelCompanyName.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' LabelCopyright
+        ' 
+        LabelCopyright.Dock = DockStyle.Fill
+        LabelCopyright.Location = New Point(7, 58)
+        LabelCopyright.Margin = New Padding(7, 0, 4, 0)
+        LabelCopyright.MaximumSize = New Size(0, 20)
+        LabelCopyright.Name = "LabelCopyright"
+        LabelCopyright.Size = New Size(452, 20)
+        LabelCopyright.TabIndex = 0
+        LabelCopyright.Text = "Copyright"
+        LabelCopyright.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' LabelVersion
+        ' 
+        LabelVersion.Dock = DockStyle.Fill
+        LabelVersion.Location = New Point(7, 29)
+        LabelVersion.Margin = New Padding(7, 0, 4, 0)
+        LabelVersion.MaximumSize = New Size(0, 20)
+        LabelVersion.Name = "LabelVersion"
+        LabelVersion.Size = New Size(452, 20)
+        LabelVersion.TabIndex = 0
+        LabelVersion.Text = "Version"
+        LabelVersion.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' LabelProductName
+        ' 
+        LabelProductName.Dock = DockStyle.Fill
+        LabelProductName.Location = New Point(7, 0)
+        LabelProductName.Margin = New Padding(7, 0, 4, 0)
+        LabelProductName.MaximumSize = New Size(0, 20)
+        LabelProductName.Name = "LabelProductName"
+        LabelProductName.Size = New Size(452, 20)
+        LabelProductName.TabIndex = 0
+        LabelProductName.Text = "Product Name"
+        LabelProductName.TextAlign = ContentAlignment.MiddleLeft
         ' 
         ' TableLayoutPanel
         ' 
@@ -50,8 +114,8 @@ Partial Class About
         TableLayoutPanel.Controls.Add(LabelProductName, 0, 0)
         TableLayoutPanel.Controls.Add(LabelVersion, 0, 1)
         TableLayoutPanel.Controls.Add(LabelCopyright, 0, 2)
-        TableLayoutPanel.Controls.Add(LabelSource, 0, 3)
-        TableLayoutPanel.Controls.Add(TextBoxLicense, 0, 4)
+        TableLayoutPanel.Controls.Add(LabelCompanyName, 0, 3)
+        TableLayoutPanel.Controls.Add(TextBoxDescription, 0, 4)
         TableLayoutPanel.Controls.Add(OKButton, 0, 5)
         TableLayoutPanel.Dock = DockStyle.Fill
         TableLayoutPanel.Location = New Point(10, 10)
@@ -67,79 +131,6 @@ Partial Class About
         TableLayoutPanel.Size = New Size(463, 298)
         TableLayoutPanel.TabIndex = 0
         ' 
-        ' LabelProductName
-        ' 
-        LabelProductName.Dock = DockStyle.Fill
-        LabelProductName.Location = New Point(7, 0)
-        LabelProductName.Margin = New Padding(7, 0, 4, 0)
-        LabelProductName.MaximumSize = New Size(0, 20)
-        LabelProductName.Name = "LabelProductName"
-        LabelProductName.Size = New Size(452, 20)
-        LabelProductName.TabIndex = 0
-        LabelProductName.Text = "Product Name"
-        LabelProductName.TextAlign = ContentAlignment.MiddleLeft
-        ' 
-        ' LabelVersion
-        ' 
-        LabelVersion.Dock = DockStyle.Fill
-        LabelVersion.Location = New Point(7, 29)
-        LabelVersion.Margin = New Padding(7, 0, 4, 0)
-        LabelVersion.MaximumSize = New Size(0, 20)
-        LabelVersion.Name = "LabelVersion"
-        LabelVersion.Size = New Size(452, 20)
-        LabelVersion.TabIndex = 0
-        LabelVersion.Text = "Version"
-        LabelVersion.TextAlign = ContentAlignment.MiddleLeft
-        ' 
-        ' LabelCopyright
-        ' 
-        LabelCopyright.Dock = DockStyle.Fill
-        LabelCopyright.Location = New Point(7, 58)
-        LabelCopyright.Margin = New Padding(7, 0, 4, 0)
-        LabelCopyright.MaximumSize = New Size(0, 20)
-        LabelCopyright.Name = "LabelCopyright"
-        LabelCopyright.Size = New Size(452, 20)
-        LabelCopyright.TabIndex = 0
-        LabelCopyright.Text = "Copyright"
-        LabelCopyright.TextAlign = ContentAlignment.MiddleLeft
-        ' 
-        ' LabelSource
-        ' 
-        LabelSource.Dock = DockStyle.Fill
-        LabelSource.Location = New Point(7, 87)
-        LabelSource.Margin = New Padding(7, 0, 4, 0)
-        LabelSource.MaximumSize = New Size(0, 20)
-        LabelSource.Name = "LabelSource"
-        LabelSource.Size = New Size(452, 20)
-        LabelSource.TabIndex = 0
-        LabelSource.Text = "Source: https://github.com/LDZPLN1/SDRTrunk-Monitor"
-        LabelSource.TextAlign = ContentAlignment.MiddleLeft
-        ' 
-        ' TextBoxLicense
-        ' 
-        TextBoxLicense.Dock = DockStyle.Fill
-        TextBoxLicense.Location = New Point(7, 119)
-        TextBoxLicense.Margin = New Padding(7, 3, 4, 3)
-        TextBoxLicense.Multiline = True
-        TextBoxLicense.Name = "TextBoxLicense"
-        TextBoxLicense.ReadOnly = True
-        TextBoxLicense.ScrollBars = ScrollBars.Both
-        TextBoxLicense.Size = New Size(452, 143)
-        TextBoxLicense.TabIndex = 0
-        TextBoxLicense.TabStop = False
-        TextBoxLicense.Text = resources.GetString("TextBoxLicense.Text")
-        ' 
-        ' OKButton
-        ' 
-        OKButton.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
-        OKButton.DialogResult = DialogResult.Cancel
-        OKButton.Location = New Point(371, 268)
-        OKButton.Margin = New Padding(4, 3, 4, 3)
-        OKButton.Name = "OKButton"
-        OKButton.Size = New Size(88, 27)
-        OKButton.TabIndex = 0
-        OKButton.Text = "&OK"
-        ' 
         ' About
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
@@ -148,18 +139,24 @@ Partial Class About
         ClientSize = New Size(483, 318)
         Controls.Add(TableLayoutPanel)
         FormBorderStyle = FormBorderStyle.FixedDialog
-        Icon = CType(resources.GetObject("$this.Icon"), Icon)
         Margin = New Padding(4, 3, 4, 3)
         MaximizeBox = False
         MinimizeBox = False
         Name = "About"
         Padding = New Padding(10)
         ShowInTaskbar = False
-        StartPosition = FormStartPosition.CenterScreen
-        Text = "About"
+        StartPosition = FormStartPosition.CenterParent
+        Text = "AboutBox1"
         TableLayoutPanel.ResumeLayout(False)
         TableLayoutPanel.PerformLayout()
         ResumeLayout(False)
     End Sub
 
+    Friend WithEvents OKButton As Button
+    Friend WithEvents TextBoxDescription As TextBox
+    Friend WithEvents LabelCompanyName As Label
+    Friend WithEvents LabelCopyright As Label
+    Friend WithEvents LabelVersion As Label
+    Friend WithEvents LabelProductName As Label
+    Friend WithEvents TableLayoutPanel As TableLayoutPanel
 End Class
